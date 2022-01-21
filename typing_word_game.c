19c5177194a0aabeb188b5649167e4d8301d6596
Matt Loera <mloera@ada.cs.pdx.edu>
2022-01-20 19:26:22 -0800

    stuff

--- typing_word_game.c
+++ typing_word_game.c
@@ -72,7 +72,8 @@ void playGame(char** hint)
         isCorrect = 1;
         timersub(&endtime,&starttime,&result);
         timeradd(&totalTime,&result,&totalTime); 
-        //printf("Correct! %ld.%06ld\n",result.tv_sec,result.tv_usec);
+        printf("Correct! %ld.%06ld\n",result.tv_sec,result.tv_usec);
+        printf("Total:%ld.%06ld\n",totalTime.tv_sec, totalTime.tv_usec);
       }else{
         isCorrect = 0;
       }
